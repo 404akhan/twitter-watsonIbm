88dffb50ec6b6c62f9874a7e7765e26bd7ced4f9
Akhan Ismailov <404akhan@gmail.com>
2016-10-23 12:51:56 +0800

    index changed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', function (req, res) {
   res
     .send('hello');
